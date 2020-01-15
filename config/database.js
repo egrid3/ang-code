131cccad5114aeef151ed04a3a452908c0b2b22c
Elvin R <egridley@ieee.org>
2020-01-15 16:38:45 -0500

    add comments and use placeholder for username and password

--- config/database.js
+++ config/database.js
@@ -1,4 +1,7 @@
 module.exports = {
-  database: 'mongodb+srv://eradmin:teCHy90-D6s@cluster0-tmehh.mongodb.net/meanauth?retryWrites=true&w=majority',
+  // connection to Mongodb Atlas with username and password (need to create another user for this database)
+
+  // local database: mongodb://localhost:27017/meanauth; production below
+  database: 'mongodb+srv://<username>:<password>@cluster0-tmehh.mongodb.net/meanauth?retryWrites=true&w=majority',
   secret: 'thesecret'
 }
